7648eccc08da750a0680677f2d2ebac4b479b74f
Lucas Campos <achcarlucas@hotmail.com>
2021-09-10 13:54:43 -0300

    Adicionar arquivos de projeto.

---
+++ Map.py
@@ -0,0 +1,21 @@
+import pygame
+
+class Game():
+    def __init__(self, screenSize):
+        pass
+
+    # função principal do jogo
+    def gameMain(self):
+        pass
+
+    # função usada para atualizar tudo da tela
+    def gameUpdate(self, deltaTime):
+        pass
+
+    # função usada para renderizar tudo da tela
+    def gameRender(self, deltaTime):
+        pass
+
+game = Game((800, 600))
+
+game.gameMain()
