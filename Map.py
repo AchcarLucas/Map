10ec13128d4c4e726d019e180685b5cf5819c081
Lucas Campos <achcarlucas@hotmail.com>
2021-09-10 14:04:54 -0300

    Código Padrão - Pygame

--- Map.py
+++ Map.py
@@ -1,21 +1,43 @@
 import pygame
 
 class Game():
-    def __init__(self, screenSize):
-        pass
+    def __init__(self, screenSize, title='Game', icon=None):
+        # inicializa as variáveis da classe
+        self.gameRunning = True
+        self.screenSize = screenSize
+        self.title = title
+        self.icon = icon
+
+        # inicializa o game
+        self.initGame()
+        
+    def initGame(self):
+        self.screen = pygame.display.set_mode(self.screenSize)
+        pygame.display.set_caption(self.title)
+        if(self.icon != None):
+            pygame.display.set_icon(self.icon)
 
     # função principal do jogo
     def gameMain(self):
-        pass
+        while self.gameRunning:
+            for event in pygame.event.get():
+                self.gameEvent(event)
+        
+    # função de eventos
+    def gameEvent(self, event):
+        if(event.type == pygame.QUIT):
+            self.gameRunning = False
+        if(event.type == pygame.KEYDOWN):
+            if(event.key == pygame.K_ESCAPE):
+                self.gameRunning = False
 
-    # função usada para atualizar tudo da tela
+    # função de atualização
     def gameUpdate(self, deltaTime):
         pass
 
-    # função usada para renderizar tudo da tela
+    # função de renderização
     def gameRender(self, deltaTime):
         pass
 
-game = Game((800, 600))
-
+game = Game((800, 600), 'Game - Map')
 game.gameMain()
